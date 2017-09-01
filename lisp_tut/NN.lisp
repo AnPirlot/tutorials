4f1ca997b739a750f9a0738b020cf8e67b602ef0
An Pirlot <api@Ans-MacBook-Pro.local>
2017-09-01 19:28:47 +0200

    update the error-margin-okp function to use recursion to check that every margin in an array is small enough

--- lisp_tut/NN.lisp
+++ lisp_tut/NN.lisp
@@ -7,7 +7,7 @@
 
 (format t "~%Starting..")
 
-(defvar *input* '(5 9 3))
+(defvar *input* '(1 1 2))
 (defvar *output* '(0 0 1))
 
 (defvar *nodes* (make-array 3 :initial-element (make-node :weight (random 1.0))))
@@ -24,36 +24,45 @@
   "Using an array of inputs and an array of outputs to train an array of nodes. This returns a copy of the array of nodes with updated weights."
   (map 'array #'update-weight-of-node nodes))
 
-;;; The function to be used to update a nodes' weight
+;;; The function to be used to update a nodes' weight.
 (defun update-weight-of-node (node)
-  "Updating the weight of the node"
+  "Updating the weight of the node."
   (return-from update-weight-of-node (make-node :weight (random 1.0))))
 
-;;; Calculate the output using an input array and an array of nodes
+;;; Calculate the output using an input array and an array of nodes.
 (defun evaluate (input nodes)
-  "Evalute the given input using the given nodes"
+  "Evalute the given input using the given nodes."
 	(map 'array #'(lambda(x) (evaluate-node input x)) nodes))
 
-;;; Calculate the output using an input array and the current weight of one node. weight*SUM(input)
+;;; Calculate the output using an input array and the current weight of one node. weight*SUM(input).
 (defun evaluate-node (input node)
-  "Evaluate to output of one node given an input"
+  "Evaluate to output of one node given an input."
 	(* (node-weight node) (reduce #'+ input)))
 
-;;; Calculate the error between a value and it's approximation
-(defun error-margin (value approximation)
-  "Calculate the error margin for every value and its approximation"
-	(map 'array #'abs (map 'array #'- value approximation)))
-
-;;; Check if the error margin is small enough
+;;; Check if every error margin in an array is small enough.
 (defun error-margin-okp (value approximation)
-  "Check if the error margin is acceptable"
+  "Check if every error margin in the array is acceptable."
 	(let ((margin (error-margin value approximation)))
-		(format t "~%the error margin for ~a: ~a" margin (reduce #'+ margin))
-		(if (< (reduce #'+ margin) 3.0) t nil)))
+		(format t "~%the error margin: ~a" margin)
+		(recursive-error-margin-okp margin (- (car (array-dimensions margin)) 1) t)))
 
-;;; Keep on training until the error margin is small enough
+;;; Calculate the error between a value and it's approximation.
+(defun error-margin (value approximation)
+  "Calculate the error margin for every value and its approximation."
+  (map 'array #'abs (map 'array #'- value approximation)))
+
+;;; Checks for an arry if the multiple error margins are small enoughresult starts as true.
+(defun recursive-error-margin-okp (margin i result)
+  "Checks multiple error margins that all margins are small enough, result starts as true."
+  (if (>= i 0)
+    (if (> (aref margin i) 0.5) 
+      (recursive-error-margin-okp margin (- i 1) (and nil result)) 
+      (recursive-error-margin-okp margin (- i 1) (and t result)))
+    result))
+
+;;; Keep on training until the error margin is small enough.
 (defun train-until-convergence (input output nodes)
-  "Keep on training the given nodes using an input and output until there weights converge"
+  "Keep on training the given nodes using an input and output until there weights converge."
     (format t "~% Still training..")
     (let ((approximation (evaluate input nodes)))
       (format t "~%approximation: ~a" approximation)
